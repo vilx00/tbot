4dfc7b270dd5bdfedb0bd276bdb688a51755981b
vilx00 <vilx00@protonmail.com>
2023-02-16 10:15:15 +0100

    project

---
+++ demo/src/main/java/com/Main/Bot.java
@@ -0,0 +1,13 @@
+package com.Main;
+
+/**
+ * Hello world!
+ *
+ */
+public class Bot 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
